48f3d38cd16107c93b2b2036ef441f1c8c0ed936
libruna <blsantos2@telefonicati.onmicrosoft.com>
2023-12-04 16:03:04 -0300

    Página de login

---
+++ monitor/lib/auth.dart
@@ -0,0 +1,125 @@
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF192655),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 36.0),
+            Text(
+              'Do Lançamento à Análise',
+              style: TextStyle(
+                fontSize: 32.0,
+                fontFamily: 'LobsterTwo',
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 36.0),
+            Text(
+              'Bem-vindo\nFaça login para continuar.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18.0,
+                fontFamily: 'JetBrainsMono',
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 36.0),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _usernameController,
+                    style: TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                      labelText: 'Usuário',
+                      labelStyle: TextStyle(color: Colors.grey[800]),
+                      fillColor: Colors.grey[200],
+                      filled: true,
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 16.0, horizontal: 16.0),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[400]!),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[400]!),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    style: TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      labelStyle: TextStyle(color: Colors.grey[800]),
+                      fillColor: Colors.grey[200],
+                      filled: true,
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 16.0, horizontal: 16.0),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[400]!),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey[400]!),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 32.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      // lógica de autenticação
+                      String username = _usernameController.text;
+                      String password = _passwordController.text;
+
+                      // Verificação
+                      if (username == 'usuarioteste' &&
+                          password == 'senhateste') {
+                        print('Login bem-sucedido');
+                      } else {
+                        print('Login falhou');
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFFFB7930),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 48.0),
+                    ),
+                    child: Text(
+                      'Entrar',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontFamily: 'JetBrainsMono',
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
